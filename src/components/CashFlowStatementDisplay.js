f1185d4904cf68e1974b725064a4d7d6ffe496ff
martin <alvarez.martinjr@gmail.com>
2021-04-21 16:08:17 -0700

    additions to income statement

--- src/components/CashFlowStatementDisplay.js
+++ src/components/CashFlowStatementDisplay.js
@@ -1,5 +1,25 @@
 import React from 'react'
 
+const formatTable =(response=[])=>{
+    if(response.length<1)return {}
+    const firstResponse = response[0];
+
+    const responseKeys = Object.keys(firstResponse);
+    const obj = {};
+    
+    responseKeys.forEach((key) => (obj[key] = []));
+    
+    response.forEach((incomeStatement) => {
+      // 5x
+      responseKeys.forEach((key) => {
+        // length amount of keys in object
+        obj[key].push(incomeStatement[key]);
+      });
+    });
+   
+    return obj
+}
+
 function CashFlowStatementDisplay(props) {
     return (
         <div>
--- src/components/IncomeStatementDisplay.js
+++ src/components/IncomeStatementDisplay.js
@@ -1,15 +1,134 @@
 import React from 'react';
+import {Table, Container} from 'react-bootstrap';
+
+const formatTable =(response=[])=>{
+    if(response.length<1)return {}
+    const firstResponse = response[0];
+
+    const responseKeys = Object.keys(firstResponse);
+    const obj = {};
+    
+    responseKeys.forEach((key) => (obj[key] = []));
+    
+    response.forEach((incomeStatement) => {
+      // 5x
+      responseKeys.forEach((key) => {
+        // length amount of keys in object
+        obj[key].push(incomeStatement[key]);
+      });
+    });
+   
+    return obj
+}
 
 
-function IncomeStatementDisplay(props) {
+function IncomeStatementDisplay({incomeList}) {
+    const incomeObj = incomeList?formatTable(incomeList):{}
+    const incomeStatementSymbol = incomeObj.symbol && incomeObj.symbol[0]
     return (
-        <div >
-                {props.incomeList.map(data =>(<div>
-                {data.netIncomeRatio??'default'}<br/>
-                {data.costOfRevenue?? 'default'}<br/>
-                {data.date}<br/>
-            </div>))}
-        </div>
+        <Container>
+            <Table responsive bordered striped>
+                <thead>
+                    <tr>
+                    <th>Income Statement for {incomeStatementSymbol}</th>
+                    {incomeObj.date?.map((value, index) => (
+                    <th key={index}>{value}</th>
+                    ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td className="font-weight-bold">Revenue</td>
+                        {incomeObj.revenue?.map((value, index) => (
+                    <td className="font-weight-bold" key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td >Cost of Goods Sold</td>
+                        {incomeObj.costOfRevenue?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Research and Development Expenses</td>
+                        {incomeObj.researchAndDevelopmentExpenses?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Selling and Marketing Expenses</td>
+                        {incomeObj.sellingAndMarketingExpenses?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Other Expenses</td>
+                        {incomeObj.otherExpenses?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td className="font-weight-bold">Total Operating Expenses</td>
+                        {incomeObj.operatingExpenses?.map((value, index) => (
+                    <td  className="font-weight-bold" key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Other Expenses</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Cost and Expenses</td>
+                        {incomeObj.costAndExpenses?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Interest Expense</td>
+                        {incomeObj.interestExpense?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Depreciation and Amortization</td>
+                        {incomeObj.depreciationAndAmortization?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td className="font-weight-bold">Operating Profit</td>
+                        {incomeObj.operatingIncome?.map((value, index) => (
+                    <td className="font-weight-bold" key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Total Other Income Expenses Net</td>
+                        {incomeObj.totalOtherIncomeExpensesNet?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td className="font-weight-bold">Income Before Tax</td>
+                        {incomeObj.incomeBeforeTax?.map((value, index) => (
+                    <td className="font-weight-bold" key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td className="table-data-style">Income Tax Expense</td>
+                        {incomeObj.incomeTaxExpense?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td className="font-weight-bold">Net Income</td>
+                        {incomeObj.netIncome?.map((value, index) => (
+                    <td className="font-weight-bold" key={index}>{value}</td>
+                    ))}
+                    </tr>
+                
+                </tbody>
+            </Table>
+        </Container>
     )
 }
 
