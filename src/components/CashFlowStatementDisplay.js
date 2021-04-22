25059422a559992483e0a495323faacf16baddbb
martin <alvarez.martinjr@gmail.com>
2021-04-22 14:12:06 -0700

    added cash flow statement from operations

--- src/components/CashFlowStatementDisplay.js
+++ src/components/CashFlowStatementDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Table, Container} from 'react-bootstrap';
 
 const formatTable =(response=[])=>{
     if(response.length<1)return {}
@@ -9,26 +10,155 @@ const formatTable =(response=[])=>{
     
     responseKeys.forEach((key) => (obj[key] = []));
     
-    response.forEach((incomeStatement) => {
+    response.forEach((cashFlowStatement) => {
       // 5x
       responseKeys.forEach((key) => {
         // length amount of keys in object
-        obj[key].push(incomeStatement[key]);
+        obj[key].push(cashFlowStatement[key]);
       });
     });
    
     return obj
 }
 
-function CashFlowStatementDisplay(props) {
+function CashFlowStatementDisplay({cashFlowList}) {
+    const cashFlowObj = cashFlowList?formatTable(cashFlowList):{}
+    const cashFlowStatementSymbol = cashFlowObj.symbol && cashFlowObj.symbol[0]
     return (
-        <div>
-                {props.cashFlowList.map(data =>(<div>
-                {data.netIncome??'default'}<br/>
-                {data.inventory??'default'}<br/>
-                {data.date}
-            </div>))}
-        </div>
+        <Container>
+            <Table responsive bordered striped>
+                <thead>
+                    <tr>
+                    <th>Cash Flow from Operating Activities {cashFlowStatementSymbol}</th>
+                    {cashFlowObj.date?.map((value, index) => (
+                    <th key={index}>{value}</th>
+                    ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td className="font-weight-bold">Net Income</td>
+                        {cashFlowObj.netIncome?.map((value, index) => (
+                    <td className="font-weight-bold" key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td >Depreciation and Amortization</td>
+                        {cashFlowObj.depreciationAndAmortization?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Stock Based Compensation</td>
+                        {cashFlowObj.stockBasedCompensation?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Deferred Income Tax</td>
+                        {cashFlowObj.deferredIncomeTax?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td className="font-weight-bold">Changes in Operating Assets and Liabilities</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Inventory</td>
+                        {cashFlowObj.inventory?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Accounts Receivables</td>
+                        {cashFlowObj.accountsReceivables?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Accounts Payables</td>
+                        {cashFlowObj.accountsPayables?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td >Change in Working Capital</td>
+                        {cashFlowObj.changeInWorkingCapital?.map((value, index) => (
+                    <td  key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Other Working Capital</td>
+                        {cashFlowObj.otherWorkingCapital?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td >Other Non-Cash Items</td>
+                        {cashFlowObj.otherNonCashItems?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td className="font-weight-bold">Net Cash Provided By Operating Activities</td>
+                        {cashFlowObj.netCashProvidedByOperatingActivities?.map((value, index) => (
+                    <td className="font-weight-bold" key={index}>{value}</td>
+                    ))}
+                    </tr>
+                </tbody>
+            </Table>
+
+            <Table responsive bordered striped>
+                <thead>
+                    <tr>
+                    <th>Investing Activities for {cashFlowStatementSymbol}</th>
+                    {cashFlowObj.date?.map((value, index) => (
+                    <th key={index}>{value}</th>
+                    ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td >Investment in Property Plant and Equipment</td>
+                        {cashFlowObj.investmentsInPropertyPlantAndEquipment?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td >Acquisitions Net</td>
+                        {cashFlowObj.acquisitionsNet?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Purchase of Investments</td>
+                        {cashFlowObj.purchasesOfInvestments?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Sales and Maturities of marketable Securities</td>
+                        {cashFlowObj.salesMaturitiesOfInvestments?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td>Other Investing Activities</td>
+                        {cashFlowObj.otherInvestingActivites?.map((value, index) => (
+                    <td key={index}>{value}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                        <td className="font-weight-bold">Net Cash Used for Investing Activities</td>
+                        {cashFlowObj.netCashUsedForInvestingActivites?.map((value, index) => (
+                    <td  className="font-weight-bold" key={index}>{value}</td>
+                    ))}
+                    </tr>   
+                </tbody>
+            </Table>
+        </Container>
+        
     )
 }
 
