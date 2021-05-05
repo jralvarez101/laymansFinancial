a02ac26e3859347c11271a2b39c15eae1fbe3d6d
martin <alvarez.martinjr@gmail.com>
2021-05-04 21:18:37 -0700

    cleanup

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import ResultsTabs from '../components/ResultsTabs';
 import Footer from '../components/Footer';
 import {Container,  Button} from 'react-bootstrap';
